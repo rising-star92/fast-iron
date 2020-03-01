251811b6173fbcd9d9a385c30d8e60b792087c73
rising-star92 <ryan.service.1001@gmail.com>
2020-03-02 04:14:02 +0530

    Django Forms

--- src/fast_iron/views.py
+++ src/fast_iron/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def home_page(request):
     context = {
@@ -19,11 +21,20 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact Page",
         "content": "Welcome to the contact page.",
+        "form": contact_form,
     }
-    return render(request, "home_page.html", context)
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
 
 
 def home_page_old(request):
