7d8b9e44e424f80589d45996933b81a5717ea352
rising-star92 <ryan.service.1001@gmail.com>
2020-03-07 11:19:04 +0530

    Custom Model Managers

--- src/products/views.py
+++ src/products/views.py
@@ -6,7 +6,6 @@ from .models import Product
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
@@ -14,6 +13,10 @@ class ProductListView(ListView):
     #         *args, **kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 
 def product_list_view(request):
@@ -32,8 +35,21 @@ class ProductDetailView(DetailView):
         context = super(ProductDetailView, self).get_context_data(
             *args, **kwargs)
         print(context)
-        context['abc'] = 123
+        # context['abc'] = 123
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
@@ -47,12 +63,17 @@ def product_detail_view(request, pk=None, *args, **kwargs):
     # except:
     #     print("huh?")
 
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesn't exist")
+    # print(instance)
     qs = Product.objects.filter(id=pk)
     # print(qs)
-    if qs.exists() and qs.count() == 1:  # len(qs)
-        instance = qs.first()
-    else:
-        raise Http404("Product doesn't exist")
+    # if qs.exists() and qs.count() == 1:  # len(qs)
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product doesn't exist")
+
     context = {
         'object': instance
     }
