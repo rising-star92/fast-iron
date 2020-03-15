9dd56192ad5173da29177e806e067cfd46672949
rising-star92 <ryan.service.1001@gmail.com>
2020-03-15 13:53:43 +0530

    Added Functionality to create Cart based on the session

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 from .models import Cart
 
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('New Cart Created')
-    return cart_obj
-
-
 def cart_home(request):
-    request.session['cart_id'] = "12"
     cart_id = request.session.get("cart_id", None)
     # if cart_id is None:
     #     cart_obj = cart_create()
@@ -19,7 +12,10 @@ def cart_home(request):
     if qs.count() == 1:
         print('Cart ID exists')
         cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
