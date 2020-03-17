990cf69141392dc43d2406ccb5bd8349a2e66b92
rising-star92 <ryan.service.1001@gmail.com>
2020-03-17 18:33:46 +0530

    Generate the Order ID

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import pre_save
+
 
 from carts.models import Cart
+from fast_iron.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -8,6 +11,8 @@ ORDER_STATUS_CHOICES = (
     ('shipped', 'Shipped'),
     ('refunded', 'Refunded')
 )
+
+# Random, Unique
 
 
 class Order(models.Model):
@@ -25,6 +30,13 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
 
 # generate the order id?
 # generate the order total?
