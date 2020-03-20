987bde30f209159f817cf302f23270f5afaac404
rising-star92 <ryan.service.1001@gmail.com>
2020-03-21 01:19:18 +0530

    Associate Billing Profile to Others

--- src/orders/models.py
+++ src/orders/models.py
@@ -2,7 +2,7 @@ import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
-
+from billing.models import BillingProfile
 from carts.models import Cart
 from fast_iron.utils import unique_order_id_generator
 
@@ -17,6 +17,8 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
+    billing_profile = models.ForeignKey(
+        BillingProfile, null=True, blank=True, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=120, blank=True)  # AB31DE3
     # billing_profile = ?
     # shipping_address
@@ -27,6 +29,7 @@ class Order(models.Model):
     shipping_total = models.DecimalField(
         default=5.99, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
