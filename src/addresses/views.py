6d5900de7353fbbe6dbbc72545785e150a6feb3d
rising-star92 <ryan.service.1001@gmail.com>
2020-03-21 23:25:50 +0530

    Reuse Addresses for Checkout

--- src/addresses/views.py
+++ src/addresses/views.py
@@ -4,6 +4,7 @@ from django.utils.http import is_safe_url
 
 from billing.models import BillingProfile
 from .forms import AddressForm
+from .models import Address
 
 
 def checkout_address_create_view(request):
@@ -20,12 +21,10 @@ def checkout_address_create_view(request):
         billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(
             request)
         if billing_profile is not None:
-            address_type = request.POST.get(
-                'address_type', 'shipping')
+            address_type = request.POST.get('address_type', 'shipping')
             instance.billing_profile = billing_profile
             instance.address_type = address_type
             instance.save()
-
             request.session[address_type + "_address_id"] = instance.id
             print(address_type + "_address_id")
         else:
@@ -34,6 +33,27 @@ def checkout_address_create_view(request):
 
         if is_safe_url(redirect_path, request.get_host()):
             return redirect(redirect_path)
-        else:
-            return redirect("cart:checkout")
+    return redirect("cart:checkout")
+
+
+def checkout_address_reuse_view(request):
+    if request.user.is_authenticated:
+        context = {}
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        if request.method == "POST":
+            print(request.POST)
+            shipping_address = request.POST.get('shipping_address', None)
+            address_type = request.POST.get('address_type', 'shipping')
+            billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(
+                request)
+            if shipping_address is not None:
+                qs = Address.objects.filter(
+                    billing_profile=billing_profile, id=shipping_address)
+                if qs.exists():
+                    request.session[address_type +
+                                    "_address_id"] = shipping_address
+                if is_safe_url(redirect_path, request.get_host()):
+                    return redirect(redirect_path)
     return redirect("cart:checkout")
