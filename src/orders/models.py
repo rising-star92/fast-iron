6dbaf3afd3fa85b817e78ad08ced7900a08345db
rising-star92 <ryan.service.1001@gmail.com>
2020-03-21 07:36:36 +0530

    Associate Addresses to Order

--- src/orders/models.py
+++ src/orders/models.py
@@ -2,6 +2,7 @@ import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
+from addresses.models import Address
 from billing.models import BillingProfile
 from carts.models import Cart
 from fast_iron.utils import unique_order_id_generator
@@ -33,9 +34,10 @@ class Order(models.Model):
     billing_profile = models.ForeignKey(
         BillingProfile, null=True, blank=True, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=120, blank=True)  # AB31DE3
-    # billing_profile = ?
-    # shipping_address
-    # billing_address
+    shipping_address = models.ForeignKey(
+        Address, related_name="shipping_address", null=True, blank=True, on_delete=models.CASCADE)
+    billing_address = models.ForeignKey(
+        Address, related_name="billing_address", null=True, blank=True, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
