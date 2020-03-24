cd7bbe877158b02702c45849b9a4b44afce90d15
rising-star92 <ryan.service.1001@gmail.com>
2020-03-25 03:00:26 +0530

    Login & Register Views

--- src/fast_iron/urls.py
+++ src/fast_iron/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
-from accounts.views import login_page, register_page, guest_register_view
+from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
-    url(r'^login/$', login_page, name='login'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^checkout/address/create/$', checkout_address_create_view,
         name='checkout_address_create'),
     url(r'^checkout/address/reuse/$', checkout_address_reuse_view,
@@ -39,7 +39,7 @@ urlpatterns = [
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^cart/', include(('carts.urls', 'carts'), namespace='cart')),
-    url(r'^register/', register_page, name='register'),
+    url(r'^register/', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include(('products.urls', 'products'), namespace='products')),
     url(r'^search/', include(('search.urls', 'search'), namespace='search')),
