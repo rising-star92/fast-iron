1a49f9f4d8a66d2c43e421ba5e52f0e6ea725761
rising-star92 <ryan.service.1001@gmail.com>
2020-03-27 12:35:15 +0530

    Payment Method View & Stripe JS

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+import stripe
+stripe.api_key = "sk_test_FQfSktUAeYgQ51Jqqc7VJ9sp00aUrZajyb"
+
+STRIPE_PUB_KEY = 'pk_test_jbYKNdGc06kkYEd5eXIXeQdu00yxQPkVex'
+
+
+def payment_method_view(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
--- src/fast_iron/urls.py
+++ src/fast_iron/urls.py
@@ -24,6 +24,7 @@ from django.views.generic import TemplateView
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
+from billing.views import payment_method_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
@@ -39,6 +40,8 @@ urlpatterns = [
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^cart/', include(('carts.urls', 'carts'), namespace='cart')),
+    url(r'^billing/payment-method/$', payment_method_view,
+        name='billng-payment-method'),
     url(r'^register/', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include(('products.urls', 'products'), namespace='products')),
