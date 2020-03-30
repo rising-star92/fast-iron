8c2f8d261322244bb467ce96230b9c050aac018f
rising-star92 <ryan.service.1001@gmail.com>
2020-03-30 12:52:05 +0530

    Prepare for HTTPs

--- src/fast_iron/settings/production.py
+++ src/fast_iron/settings/production.py
@@ -160,3 +160,17 @@ STATIC_ROOT = os.path.join(os.path.dirname(
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "STATIC_CDN", "media_root")
+
+
+# Let's Encrypt ssl/tls https
+
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
+
