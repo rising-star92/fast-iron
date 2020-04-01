78b4de3d736a2ae13a7bd7b777a7b6557274b3fe
rising-star92 <ryan.service.1001@gmail.com>
2020-04-01 19:04:13 +0530

    Filter by Range of Time

--- src/orders/models.py
+++ src/orders/models.py
@@ -10,18 +10,53 @@ from addresses.models import Address
 from billing.models import BillingProfile
 from carts.models import Cart
 from fast_iron.utils import unique_order_id_generator
+from products.models import Product
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
     ('paid', 'Paid'),
     ('shipped', 'Shipped'),
-    ('refunded', 'Refunded')
+    ('refunded', 'Refunded'),
 )
 
 
 class OrderManagerQuerySet(models.query.QuerySet):
     def recent(self):
         return self.order_by("-updated", "-timestamp")
+
+    def get_sales_breakdown(self):
+        recent = self.recent().not_refunded()
+        recent_data = recent.totals_data()
+        recent_cart_data = recent.cart_data()
+        shipped = recent.not_refunded().by_status(status='shipped')
+        shipped_data = shipped.totals_data()
+        paid = recent.by_status(status='paid')
+        paid_data = paid.totals_data()
+        data = {
+            'recent': recent,
+            'recent_data': recent_data,
+            'recent_cart_data': recent_cart_data,
+            'shipped': shipped,
+            'shipped_data': shipped_data,
+            'paid': paid,
+            'paid_data': paid_data
+        }
+        return data
+
+    def by_weeks_range(self, weeks_ago=7, number_of_weeks=2):
+        if number_of_weeks > weeks_ago:
+            number_of_weeks = weeks_ago
+        days_ago_start = weeks_ago * 7  # days_ago_start = 49
+        # days_ago_end = 49 - 14 = 35
+        days_ago_end = days_ago_start - (number_of_weeks * 7)
+        start_date = timezone.now() - datetime.timedelta(days=days_ago_start)
+        end_date = timezone.now() - datetime.timedelta(days=days_ago_end)
+        return self.by_range(start_date, end_date=end_date)
+
+    def by_range(self, start_date, end_date=None):
+        if end_date is None:
+            return self.filter(updated__gte=start_date)
+        return self.filter(updated__gte=start_date).filter(updated__lte=end_date)
 
     def by_date(self):
         now = timezone.now() - datetime.timedelta(days=9)
@@ -61,12 +96,17 @@ class OrderManager(models.Manager):
     def new_or_get(self, billing_profile, cart_obj):
         created = False
         qs = self.get_queryset().filter(
-            billing_profile=billing_profile, cart=cart_obj, active=True, status='created')
+            billing_profile=billing_profile,
+            cart=cart_obj,
+            active=True,
+            status='created'
+        )
         if qs.count() == 1:
             obj = qs.first()
         else:
             obj = self.model.objects.create(
-                billing_profile=billing_profile, cart=cart_obj)
+                billing_profile=billing_profile,
+                cart=cart_obj)
             created = True
         return obj, created
 
@@ -120,18 +160,36 @@ class Order(models.Model):
         return new_total
 
     def check_done(self):
+        shipping_address_required = not self.cart.is_digital
+        shipping_done = False
+        if shipping_address_required and self.shipping_address:
+            shipping_done = True
+        elif shipping_address_required and not self.shipping_address:
+            shipping_done = False
+        else:
+            shipping_done = True
         billing_profile = self.billing_profile
-        shipping_address = self.shipping_address
         billing_address = self.billing_address
         total = self.total
-        if billing_profile and shipping_address and billing_address and total > 0:
+        if billing_profile and shipping_done and billing_address and total > 0:
             return True
         return False
 
+    def update_purchases(self):
+        for p in self.cart.products.all():
+            obj, created = ProductPurchase.objects.get_or_create(
+                order_id=self.order_id,
+                product=p,
+                billing_profile=self.billing_profile
+            )
+        return ProductPurchase.objects.filter(order_id=self.order_id).count()
+
     def mark_paid(self):
-        if self.check_done():
-            self.status = "paid"
-            self.save()
+        if self.status != 'paid':
+            if self.check_done():
+                self.status = "paid"
+                self.save()
+                self.update_purchases()
         return self.status
 
 
@@ -176,5 +234,55 @@ def post_save_order(sender, instance, created, *args, **kwargs):
 
 post_save.connect(post_save_order, sender=Order)
 
-# generate the order id?
-# generate the order total?
+
+class ProductPurchaseQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(refunded=False)
+
+    def digital(self):
+        return self.filter(product__is_digital=True)
+
+    def by_request(self, request):
+        billing_profile, created = BillingProfile.objects.new_or_get(request)
+        return self.filter(billing_profile=billing_profile)
+
+
+class ProductPurchaseManager(models.Manager):
+    def get_queryset(self):
+        return ProductPurchaseQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def digital(self):
+        return self.get_queryset().active().digital()
+
+    def by_request(self, request):
+        return self.get_queryset().by_request(request)
+
+    def products_by_id(self, request):
+        qs = self.by_request(request).digital()
+        ids_ = [x.product.id for x in qs]
+        return ids_
+
+    def products_by_request(self, request):
+        ids_ = self.products_by_id(request)
+        products_qs = Product.objects.filter(id__in=ids_).distinct()
+        return products_qs
+
+
+class ProductPurchase(models.Model):
+    order_id = models.CharField(max_length=120)
+    # billingprofile.productpurchase_set.all()
+    billing_profile = models.ForeignKey(
+        BillingProfile, on_delete=models.CASCADE)
+    # product.productpurchase_set.count()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    refunded = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = ProductPurchaseManager()
+
+    def __str__(self):
+        return self.product.title
