d2daddc44f32c4cfdbc203b94b357704588c9be9
rising-star92 <ryan.service.1001@gmail.com>
2020-04-02 16:00:45 +0530

    updated contact page text

--- src/fast_iron/views.py
+++ src/fast_iron/views.py
@@ -30,7 +30,7 @@ def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact",
-        "content": " Welcome to the contact page.",
+        "content": "If you would like to get in touch, please send us an email",
         "form": contact_form,
     }
     if contact_form.is_valid():
